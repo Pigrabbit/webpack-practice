a55785bec5a73ef056c45f7e646904a5cf6559b3
Pigrabbit <donghyuk1003@gmail.com>
2020-07-13 11:19:28 +0900

    Add webpack loader

--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@
 import hello_word from "./hello";
 import world_word from "./world";
 
+import css from './style.css';
+
 document.querySelector("#root").innerHTML = `${hello_word}, ${world_word}`;
+console.log('css', css);
